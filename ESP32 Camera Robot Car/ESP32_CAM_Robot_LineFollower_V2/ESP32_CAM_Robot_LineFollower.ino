f90fd6dc1d1dfd06bdc7c3183006c8f1a16f4987
PTSangk <106540883+PTSangk@users.noreply.github.com>
2024-10-09 23:32:26 +0700

    Update ESP32_CAM_Robot_LineFollower.ino

--- ESP32 Camera Robot Car/ESP32_CAM_Robot_LineFollower_V2/ESP32_CAM_Robot_LineFollower.ino
+++ ESP32 Camera Robot Car/ESP32_CAM_Robot_LineFollower_V2/ESP32_CAM_Robot_LineFollower.ino
@@ -1,66 +1,336 @@
-struct MOTOR_PINS
-{
-  int pinEn;  
-  int pinIN1;
-  int pinIN2;    
-};
+#include "esp_camera.h"
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <iostream>
+#include <sstream>
 
-std::vector<MOTOR_PINS> motorPins = 
-{
-  {12, 13, 15},  //RIGHT_MOTOR Pins (EnA, IN1, IN2)
- {12, 14, 3},  //LEFT_MOTOR  Pins (EnB, IN3, IN4)
- 
-};
-// 28June24 #define LIGHT_PIN 4
+#include "pin_define.h"
+#include "index.h"
+#include "fb_gfx.h"
+#include "soc/soc.h"           // disable brownout problems
+#include "soc/rtc_cntl_reg.h"  // disable brownout problems
+#include "driver/rtc_io.h"
 
-#define UP 1
-#define DOWN 2
-#define LEFT 3
-#define RIGHT 4
-#define STOP 0
+int mode = 0;
+//int ledstatus = 0;
+//std::string mode = "manual";
 
-#define RIGHT_MOTOR 0
-#define LEFT_MOTOR 1
+void rotateMotor(int motorNumber, int motorDirection) {
+  if (motorDirection == FORWARD) {
+    digitalWrite(motorPins[motorNumber].pinIN1, HIGH);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);
+  } else if (motorDirection == BACKWARD) {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, HIGH);
+  } else {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);
+  }
+}
 
-#define FORWARD 1
-#define BACKWARD -1
+void moveCar(int inputValue) {
+  Serial.printf("Got value as %d\n", inputValue);
+  switch (inputValue) {
 
-const int PWMFreq = 1000; /* 1 KHz */
-const int PWMResolution = 8;
-const int PWMSpeedChannel = 12; // change PWM to GPIO 12
-const int PWMLightChannel = 2;
+    case UP:
+      rotateMotor(RIGHT_MOTOR, FORWARD);
+      rotateMotor(LEFT_MOTOR, FORWARD);
+      break;
 
-int slRead = 0;
-int srRead = 0;
+    case DOWN:
+      rotateMotor(RIGHT_MOTOR, BACKWARD);
+      rotateMotor(LEFT_MOTOR, BACKWARD);
+      break;
 
-//Camera related constants
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+    case LEFT:
+      rotateMotor(RIGHT_MOTOR, FORWARD);
+      rotateMotor(LEFT_MOTOR, BACKWARD);
+      break;
 
-#define  SensorRIGHT            1  // RIGHT sensor 
-#define  SensorLEFT             4  // LEFT sensor 
-#define  ledPin                 2  // LED input
-//https://i0.wp.com/randomnerdtutorials.com/wp-content/uploads/2018/08/esp32-pinout-chip-ESP-WROOM-32.png?quality=100&strip=all&ssl=1
+    case RIGHT:
+      rotateMotor(RIGHT_MOTOR, BACKWARD);
+      rotateMotor(LEFT_MOTOR, FORWARD);
+      break;
+
+    case STOP:
+      rotateMotor(RIGHT_MOTOR, STOP);
+      rotateMotor(LEFT_MOTOR, STOP);
+      break;
+
+    default:
+      rotateMotor(RIGHT_MOTOR, STOP);
+      rotateMotor(LEFT_MOTOR, STOP);
+      break;
+  }
+}
+
+void handleRoot(AsyncWebServerRequest *request) {
+  request->send_P(200, "text/html", htmlHomePage);
+}
+
+void handleNotFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "File Not Found");
+}
+
+void onCarInputWebSocketEvent(AsyncWebSocket *server,
+                              AsyncWebSocketClient *client,
+                              AwsEventType type,
+                              void *arg,
+                              uint8_t *data,
+                              size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      moveCar(STOP);
+      mode = 0;
+      ledcWrite(PWMLightChannel, 0);
+      break;
+    case WS_EVT_DATA:
+      AwsFrameInfo *info;
+      info = (AwsFrameInfo *)arg;
+      if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+        std::string myData = "";
+        myData.assign((char *)data, len);
+        std::istringstream ss(myData);
+        std::string key, value;
+        std::getline(ss, key, ',');
+        std::getline(ss, value, ',');
+        Serial.printf("Key [%s] Value[%s]\n", key.c_str(), value.c_str());
+        int valueInt = atoi(value.c_str());
+        if (key == "MoveCar" && mode == 0) {
+          //if (key == "MoveCar") {
+          moveCar(valueInt);
+        } else if (key == "Speed") {
+          if (mode == 0) {
+            ledcWrite(PWMSpeedChannel, valueInt);
+          } else if (mode == 1) {
+            ledcWrite(PWMSpeedChannel, 90);
+          }  //95
+             //ledcWrite(PWMSpeedChannel, valueInt);
+        } else if (key == "LED") {
+          ledcWrite(PWMLightChannel, valueInt);
+        } else if (key == "Mode") {
+          mode = valueInt;
+          moveCar(STOP);
+          //Serial.printf("mode %d \n", mode);
+          //if(mode == 0){ledcWrite(PWMSpeedChannel, 150);moveCar(STOP); }
+          //else if(mode == 1) { moveCar(STOP);} //95
+
+          //mode = value.c_str();
+          //Serial.printf("mode %s \n", mode);
+        }
+        else if(key == "led"){
+          //ledstatus = valueInt;
+          //digitalWrite(ledPin,valueInt);
+          delay(200);
+        }
+      }
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+    default:
+      break;
+  }
+}
+
+void onCameraWebSocketEvent(AsyncWebSocket *server,
+                            AsyncWebSocketClient *client,
+                            AwsEventType type,
+                            void *arg,
+                            uint8_t *data,
+                            size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      cameraClientId = client->id();
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      cameraClientId = 0;
+      break;
+    case WS_EVT_DATA:
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+    default:
+      break;
+  }
+}
+
+void setupCamera() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
 
 
-const char* ssid     = "robot_car_09";
-const char* password = "123456789";
 
-AsyncWebServer server(80);
-AsyncWebSocket wsCamera("/Camera");
-AsyncWebSocket wsCarInput("/CarInput");
-uint32_t cameraClientId = 0;
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  } else {
+    wsCamera.onEvent(onCameraWebSocketEvent);
+    server.addHandler(&wsCamera);
+  }
+  // 0 = disable , 1 = enable
+
+
+  if (psramFound()) {
+    heap_caps_malloc_extmem_enable(20000);
+    Serial.printf("PSRAM initialized. malloc to take memory from psram above this size");
+  }
+
+  //ESP32-CAM OV2640 Camera Settings for rotate 90
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+}
+
+void sendCameraPicture() {
+
+  if (cameraClientId == 0) {
+    return;
+  }
+  unsigned long startTime1 = millis();
+  //capture a frame
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Frame buffer could not be acquired");
+    return;
+  }
+
+  unsigned long startTime2 = millis();
+  wsCamera.binary(cameraClientId, fb->buf, fb->len);
+  esp_camera_fb_return(fb);
+
+  //Wait for message to be delivered
+  while (true) {
+    AsyncWebSocketClient *clientPointer = wsCamera.client(cameraClientId);
+    if (!clientPointer || !(clientPointer->queueIsFull())) {
+      break;
+    }
+    delay(1);
+  }
+
+  unsigned long startTime3 = millis();
+  Serial.printf("Time taken Total: %d|%d|%d\n", startTime3 - startTime1, startTime2 - startTime1, startTime3 - startTime2);
+}
+
+
+void setUpPinModes() {
+  //Set up PWM
+  ledcSetup(PWMSpeedChannel, PWMFreq, PWMResolution);
+  ledcSetup(PWMLightChannel, PWMFreq, PWMResolution);
+
+  for (int i = 0; i < motorPins.size(); i++) {
+    pinMode(motorPins[i].pinEn, OUTPUT);
+    pinMode(motorPins[i].pinIN1, OUTPUT);
+    pinMode(motorPins[i].pinIN2, OUTPUT);
+
+    /* Attach the PWM Channel to the motor enb Pin */
+    ledcAttachPin(motorPins[i].pinEn, PWMSpeedChannel);
+  }
+  moveCar(STOP);
+
+  pinMode(ledPin, OUTPUT);
+  ledcAttachPin(ledPin, PWMLightChannel);
+  //pinMode(PWDN_GPIO_NUM, OUTPUT);
+}
+
+
+void setup(void) {
+
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  // disable brownout detector
+    //declaring pin types
+  pinMode(SensorRIGHT, INPUT);
+  pinMode(SensorLEFT, INPUT);
+  //pinMode(ledPin, OUTPUT);
+  setUpPinModes();
+  //Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/", HTTP_GET, handleRoot);
+  server.onNotFound(handleNotFound);
+
+  wsCamera.onEvent(onCameraWebSocketEvent);
+  server.addHandler(&wsCamera);
+
+  wsCarInput.onEvent(onCarInputWebSocketEvent);
+  server.addHandler(&wsCarInput);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  setupCamera();
+  //sendCameraPicture();
+}
+
+void loop() {
+
+  wsCamera.cleanupClients();
+  wsCarInput.cleanupClients();
+  sendCameraPicture();
+  //delay(100);  // Adjust delay based on streaming frame rate
+  //Serial.printf("SPIRam Total heap %d, SPIRam Free Heap %d\n", ESP.getPsramSize(), ESP.getFreePsram());
+  slRead = digitalRead(SensorLEFT);
+  srRead = digitalRead(SensorRIGHT);
+  if (mode == 1) {
+    if (slRead == 0 && srRead == 0) {
+      //Forward
+      moveCar(UP);
+      //delay(100);
+    }
+    //line detected by left sensor
+    if (slRead == 0 && !srRead == 0) {
+      //turn left
+      moveCar(LEFT);
+      delay(300);  //150
+    }
+    //line detected by right sensor
+    if (!slRead == 0 && srRead == 0) {
+      //turn right
+      moveCar(RIGHT);
+      delay(300);
+    }
+    //line detected by none
+    if (!slRead == 0 && !srRead == 0) {
+      //stop
+      moveCar(STOP);
+      //delay(100);
+    }
+  }
+}
