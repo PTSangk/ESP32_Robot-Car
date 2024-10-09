9464f8b2c683af22eeccdefb0712ffe90be03193
PTSangk <106540883+PTSangk@users.noreply.github.com>
2024-10-09 23:18:39 +0700

    Update pin_define.h

--- ESP32 Camera Robot Car/ESP32_CAM_Robot_LineFollower_V2/pin_define.h
+++ ESP32 Camera Robot Car/ESP32_CAM_Robot_LineFollower_V2/pin_define.h
@@ -8,7 +8,7 @@ struct MOTOR_PINS
 std::vector<MOTOR_PINS> motorPins = 
 {
   {12, 13, 15},  //RIGHT_MOTOR Pins (EnA, IN1, IN2)
- {12, 14, 2},  //LEFT_MOTOR  Pins (EnB, IN3, IN4)
+ {12, 14, 3},  //LEFT_MOTOR  Pins (EnB, IN3, IN4)
  
 };
 // 28June24 #define LIGHT_PIN 4
@@ -27,8 +27,8 @@ std::vector<MOTOR_PINS> motorPins =
 
 const int PWMFreq = 1000; /* 1 KHz */
 const int PWMResolution = 8;
-const int PWMSpeedChannel = 2;
-const int PWMLightChannel = 3;
+const int PWMSpeedChannel = 3;
+const int PWMLightChannel = 2;
 
 int slRead = 0;
 int srRead = 0;
@@ -53,7 +53,7 @@ int srRead = 0;
 
 #define  SensorRIGHT            1  // RIGHT sensor 
 #define  SensorLEFT             4  // LEFT sensor 
-#define  ledPin                 3  // LED input
+#define  ledPin                 2  // LED input
 //https://i0.wp.com/randomnerdtutorials.com/wp-content/uploads/2018/08/esp32-pinout-chip-ESP-WROOM-32.png?quality=100&strip=all&ssl=1
 
 
